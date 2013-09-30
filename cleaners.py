3850b7be3694875d4787d1b4e299a65f8bec2096
Max Kolysh <maxko87@gmail.com>
2013-09-30 01:25:16 -0400

    fixed address matching

--- cleaners.py
+++ cleaners.py
@@ -4,10 +4,10 @@ address_mappings = {
     " street": " st",
     " avenue": " ave",
     " road": " rd",
-    " east ": " e ",
-    " west ": " w ",
-    " north ": " n ",
-    " south ": " s ",
+    " east ": " e. ",
+    " west ": " w. ",
+    " north ": " n. ",
+    " south ": " s. ",
 }
 
 def clean_phone(phone_number):
@@ -16,12 +16,21 @@ def clean_phone(phone_number):
 def clean_address(address):
     address = address.lower()
     address = re.sub(r"[^a-zA-Z0-9\ ]", "", address)
-    address = re.sub(r"([\d]{1,3})(st)|(nd)|(rd)|(th)", r"", address)
+    address = re.sub(r"([\d]{1,3})(st)|(nd)|(rd)|(th)", "", address)
     for key, value in address_mappings.items():
         address = re.sub(key, value, address)
     return address
+
+def clean_website(website):
+	website = re.sub("https?://", "", website)
+	website = re.sub("www.", "", website)
+	return website
 
 
 # print "start\n\n"
 # print clean_address("W. 97th St.")
 # print clean_address("W. 3rd street")
+# print clean_website("https://www.ina.com")
+# print clean_website("http://www.ina.com")
+# print clean_website("www.ina.com")
+# print clean_website("https://ina.com")
