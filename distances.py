ea07ce370c6cd31f1f6431b4c7037fe127f26de3
Max Kolysh <maxko87@gmail.com>
2013-09-29 23:37:12 -0400

    more distances

--- distances.py
+++ distances.py
@@ -17,6 +17,8 @@ Defines a distance metric for every field in the dataset.
 
 '''
 
+lat_lng_threshold = .5
+
 class Distance():
     def distance(a,b):
         pass
@@ -25,8 +27,7 @@ class Distance():
         n, m = len(a), len(b)
         if n > m:
             a,b = b,a
-            n,m = m,n
-            
+            n,m = m,n 
         current = range(n+1)
         for i in range(1,m+1):
             previous, current = current, [i]+[0]*n
@@ -35,9 +36,21 @@ class Distance():
                 change = previous[j-1]
                 if a[j-1] != b[i-1]:
                     change = change + 1
-                current[j] = min(add, delete, change)
-                
-        return current[n]
+                current[j] = min(add, delete, change) 
+        # 0->1, m->0
+        return 1 - (current[n] / m)
+
+class Levenshtein(Distance)
+    def distance(a,b,attr):
+        if (a.attr[attr] == "" or b.attr[attr] == ""):
+            return None
+        return levenshtein(a.attr[attr]), b.attr[attr]))
+
+class ExactMatch(Distance):
+    def distance(a,b,attr):
+        if (a.attr[attr] == "" or b.attr[attr] == ""):
+            return None
+        return 1 if a.attr[attr] == b.attr[attr] else return 0
 
 class LatLng(Distance):
     def distance(a,b):
@@ -45,20 +58,36 @@ class LatLng(Distance):
         lat_b = float(b.attr['lattitude'])
         lng_a = float(a.attr['longitude'])
         lng_b = float(b.attr['longitude'])
-        return (lat_a-lat_b)**2 + (lng_a-lng_b)**2
+        if ((lat_a == "" or lat_b == "") and (lng_a == "" or lng_b == ""):
+            return None
+        dist = 0 
+        if (lat_a != "" and lat_b != ""):
+            dist += (lat_a-lat_b)**2
+        if (lng_a != "" and lng_b != ""):
+            dist += (lng_a-lng_b)**2
+        if dist > lat_lng_threshold:
+            return 0
+        return 1 - (dist / lat_lng_threshold)
 
-class Name(Distance):
+class Name(Levenshtein):
     def distance(a,b):
-        return levenshtein(a.attr['name']), b.attr['name']))
+        return distance(a,b,"name")
 
-class Address(Distance):
+class Address(Levenshtein):
     def distance(a,b):
-        return levenshtein(a.attr['street_address']), b.attr['street_address']))
+        return distance(a,b,"street_address")
 
-class Website(Distance):
+class Website(Levenshtein):
     def distance(a,b):
-        return levenshtein(a.attr['website']), b.attr['website']))
+        return distance(a,b,"website")
 
+class PostalCode(ExactMatch)
+    def distance(a,b):
+        return distance(a,b,"postal_code")
+
+class Phone(ExactMatch)
+    def distance(a,b):
+        return distance(a,b,"phone")
 
 
 
