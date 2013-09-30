3db9f7381e6c751eecf7b11bf23534777b72ad3a
Sharon Hao <hao.sharon@gmail.com>
2013-09-29 21:45:29 -0400

    add training data

---
+++ measure.py
@@ -0,0 +1,14 @@
+import io, sys, string, os
+
+def measure(base_file_name, test_file_name):
+	base_file = open(base_file_name)
+	test_file = open(test_file_name)
+
+
+
+
+if __name__ == '__main__':
+	base_file = sys.argv[1]
+	test_file = sys.argv[2]
+	measure(base_file, test_file)
+	
